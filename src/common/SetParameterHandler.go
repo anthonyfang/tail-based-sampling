d65ddcea47136eee26329e506dd015015d0b06f0
saiwas <johnnysaiwas@gmail.com>
2020-11-29 17:00:16 +0800

    Add first version logic

--- src/common/SetParameterHandler.go
+++ src/common/SetParameterHandler.go
@@ -1,19 +1,20 @@
 package common
 
 import (
-	"bufio"
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-	"strings"
+    "bufio"
+    "fmt"
+    "log"
+    "net/http"
+    "os"
+    "strings"
+    "time"
 
-	"github.com/gofiber/fiber/v2"
+    "github.com/gofiber/fiber/v2"
 )
 
 // SetParameterPostHandler is use for handling the SetParameterHandler endpoint
 func SetParameterPostHandler(c *fiber.Ctx) error {
-	type Request struct {
+    type Request struct {
         Port string `json:"port"`
     }
     var body Request
@@ -30,7 +31,8 @@ func SetParameterPostHandler(c *fiber.Ctx) error {
     serverPort := GetEnvDefault("SERVER_PORT", "8002")
 
     if serverPort != "8002" {
-        go fetchData(body.Port)
+        url := getURL(body.Port)
+        go fetchData(url)
     }
 
     return c.SendString(fmt.Sprintf("OK! Upload server port is: %v", body.Port))
@@ -38,20 +40,60 @@ func SetParameterPostHandler(c *fiber.Ctx) error {
 
 // SetParameterGetHandler is use for handling the SetParameterHandler endpoint
 func SetParameterGetHandler(c *fiber.Ctx) error {
-	port := c.Params("port")
+    port := c.Query("port")
+    
     os.Setenv("UPLOAD_SERVER_PORT", port)
 
     serverPort := GetEnvDefault("SERVER_PORT", "8002")
 
     if serverPort != "8002" {
-        go fetchData(port)
+        url := getURL(port)
+        go fetchData(url)
     }
 
     return c.SendString(fmt.Sprintf("OK! Upload server port is: %v", port))
 }
 
 // fetchData is use for fetching the data file from datasource server
-func fetchData(port string){
+func fetchData(url string){
+    fmt.Println("################# : fetchingData", time.Now())
+    if url != "" {
+        resp, err := http.Get(url)
+        if err != nil {
+            log.Fatalln(err)
+        }
+
+        defer resp.Body.Close()
+        scanner := bufio.NewScanner(resp.Body)
+        buf := make([]byte, 64*1024)
+        scanner.Buffer(buf, bufio.MaxScanTokenSize)
+        // process(resp.Body)
+
+        // scanner := bufio.NewScanner(f)
+
+        i := 0
+        lastCleanupIndex := i
+        for scanner.Scan() {
+            recordString := scanner.Text()
+            pushToCacheQueue(recordString, i)
+            // fmt.Println(scanner.Text())
+
+            // tigger the cleanup worker every 20000 record
+            if i - lastCleanupIndex > 20000 {
+                go cleanUpWorker(i)
+            }
+            i++
+        }
+        
+        fmt.Println("xxxxxxxxxxxxxxxxxx: ", i)
+        for key, record := range CacheQueue {
+            fmt.Println("Key:", key, "=>", "Element:",record)
+        }
+    }
+    fmt.Println("################# : fetchingData END", time.Now())
+}
+
+func getURL(port string) string {
     var url string
     switch currentServerPort := GetEnvDefault("SERVER_PORT", ""); currentServerPort {
     case "8000":
@@ -61,43 +103,43 @@ func fetchData(port string){
     default:
         url = ""
     }
-    
-    if url != "" {
-        resp, err := http.Get(url)
-        if err != nil {
-            log.Fatalln(err)
-        }
 
-        defer resp.Body.Close()
-        scanner := bufio.NewScanner(resp.Body)
+    return url
+}
 
-        i := 0
-        for scanner.Scan() {
-            recordString := scanner.Text()
-            record := strings.Split(recordString, "|")
-            traceID := record[0]
+func pushToCacheQueue(recordString string, currentLineNo int) {
+    CQLocker.Lock()
+    record := strings.Split(recordString, "|")
+    traceID := record[0]
 
-            // validate error record
-            hasError := false
-            if len(record) > 8 {
-                hasError = isErrorRecord(record[8])
-            }
-
-            // add the line to cacheQueue
-            data := &RecordTemplate{hasError, i, false, []string{}}
-            if len(CacheQueue[traceID].records) > 0 {
-                data = &RecordTemplate{hasError, i, false, CacheQueue[traceID].records}
-            }
-            data.UpdateRecord(recordString)
-
-            fmt.Println(scanner.Text())
-
-            i++
-        }
+    // validate error record
+    hasError := false
+    if len(record) > 8 {
+        hasError = isErrorRecord(record[8])
     }
+
+    // add the line to cacheQueue
+    data := &RecordTemplate{hasError, currentLineNo, false, []string{}}
+    if CacheQueue[traceID] != nil {
+        data = &RecordTemplate{hasError, currentLineNo, false, CacheQueue[traceID].records}
+    }
+    data.UpdateRecord(recordString)
+    CacheQueue[traceID] = data
+    CQLocker.Unlock()
 }
 
 func isErrorRecord(tags string) bool {
-    result := (!strings.Contains(tags, "http.status_code=200") || strings.Contains(tags, ""))
+    result := (!strings.Contains(tags, "http.status_code=200") || strings.Contains(tags, "error=1"))
     return result
+}
+
+func cleanUpWorker(currentLineNo int) {
+    CQLocker.Lock()
+    for key, record := range CacheQueue {
+        if (currentLineNo - record.startLineNO > 2000 && !record.hasError || record.hasReport) {
+            // fmt.Println("---------------------------- Delete:", key)
+            delete(CacheQueue, key)
+        }
+    }
+    CQLocker.Unlock()
 }
